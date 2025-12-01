6cf23faac6f7372673ac586b8256a9d5118c49ce
Dilli-Rani <dillirani0305@gmail.com>
2025-12-01 10:43:46 +0530

    Add files via upload

---
+++ TrackPhone.py
@@ -0,0 +1,13 @@
+import phonenumbers
+from phonenumbers import geocoder
+
+phone_number1 = phonenumbers.parse("+911234567890")
+phone_number2 = phonenumbers.parse("+12025550123")
+phone_number3 = phonenumbers.parse("+12136574429")
+phone_number4 = phonenumbers.parse("+201234567890")
+
+print("\nPhone Numbers Location\n")
+print(geocoder.description_for_number(phone_number1, "en"))
+print(geocoder.description_for_number(phone_number2, "en"))
+print(geocoder.description_for_number(phone_number3, "en"))
+print(geocoder.description_for_number(phone_number4, "en"))
